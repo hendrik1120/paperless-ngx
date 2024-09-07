06226f29fcf435b4ef9500f217083d8114a6d72f
hendrik1120 <89412959+hendrik1120@users.noreply.github.com>
2024-09-07 16:54:50 +0200

    fix linting check

--- src/paperless/strategies.py
+++ src/paperless/strategies.py
@@ -6,7 +6,6 @@ from rest_framework.authtoken.models import Token
 
 
 class DrfTokenStrategy(SessionTokenStrategy):
-
     def create_access_token(self, request: HttpRequest) -> Optional[str]:
         token, _ = Token.objects.get_or_create(user=request.user)
         return token.key
